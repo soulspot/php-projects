f34d7e66ae68f9d3c0e9a4bcd584ffa497d2aef2
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2022-04-26 22:14:24 +0530

    adding hashing on password filed

--- login.php
+++ login.php
@@ -6,15 +6,22 @@
         $username=$_POST["username"];
         $password=$_POST["password"];
         $exsist=false;
-        $sql="SELECT * FROM users where username='$username' AND  password='$password'";
+        $sql="SELECT * FROM users where username='$username'";
         $result=mysqli_query($connection,$sql);
         $num=mysqli_num_rows($result);
         if($num==1){
-            $login=true;
-            session_start();
-            $_SESSION['loggedin']=true;
-            $_SESSION['username']=$username;
-            header('location: welcome.php');  
+          while($row=mysqli_fetch_assoc($result)){
+            if(password_verify($password,$row['password'])){
+              $login=true;
+              session_start();
+              $_SESSION['loggedin']=true;
+              $_SESSION['username']=$username;
+              header('location: welcome.php');  
+            }
+            else{
+              $showError="Invalid Credentials";
+            }
+          }
         }
         else{
           $showError="Invalid Credentials";
--- signup.php
+++ signup.php
@@ -16,7 +16,8 @@
         }
         else{
             if($password==$cpassword){
-                $sql="INSERT INTO `users` (`username`, `password`, `dt`) VALUES ('$username', '$password', current_timestamp())";
+                $hash=password_hash($password, PASSWORD_DEFAULT);
+                $sql="INSERT INTO `users` (`username`, `password`, `dt`) VALUES ('$username', '$hash', current_timestamp())";
                 $result=mysqli_query($connection,$sql);
                 if($result){
                     $showAlert=true;
